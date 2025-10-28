da218babb800efb5362cd71de58dd78fce9421f4
Noam Mendoza <noamalfredo@gmail.com>
2025-10-28 11:13:42 -0600

    Locate executable files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -7,7 +8,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         
-        //String [] commands = {"echo", "type", "exit"};
         ArrayList<String> commands = new ArrayList<>(Arrays.asList("echo", "type", "exit"));
         while (true) { 
             System.out.print("$ ");
@@ -20,22 +20,40 @@ public class Main {
                 System.exit(0);
             }
             else if (Detector[0].equals("echo")) {
-                String echoOutput = input.substring(5);
-                System.out.println(echoOutput);
+                System.out.println(echo(input));
             }
             else if (Detector[0].equals("type")) {
-                if(commands.contains(Detector[1])){
-                    System.out.println(Detector[1] + " is a shell builtin");
-                }else{
-                    System.out.println(Detector[1] + ": not found");
-                }
+                System.out.println(type(commands,  Detector));
             }else{
                 System.out.println(input + ": command not found");
             }
             
 
-           
+           scanner.close();
         }
         
+    }
+
+    public static String echo(String input){
+        String echoOutput = input.substring(5);
+        return echoOutput;
+    }
+
+    public static String type(ArrayList<String> commands, String [] Detector){
+        String path = System.getenv("PATH");
+        String [] path_commands = path.split(":");
+        if(commands.contains(Detector[1])){
+            return Detector[1] + " is a shell builtin";
+        }else{
+            for (String dir : path_commands) {
+                File file = new File(dir, Detector[1]);
+                if (file.exists() && file.canExecute()) {
+                    return Detector[1]+" is "+file.getAbsolutePath();
+                }else{
+                    return Detector[1] + ": not found";
+                }
+            }
+        }
+        return "";
     }
 }
