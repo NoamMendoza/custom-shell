836dee59c219c009203304161de2787cadd0a712
Noam Mendoza <noamalfredo@gmail.com>
2025-10-27 22:31:57 -0600

    Implement type

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,11 +16,11 @@ public class Main {
             if (input.equals("exit 0")) {
                 System.exit(0);
             }
-            if (Detector[0].equals("echo")) {
+            else if (Detector[0].equals("echo")) {
                 String echoOutput = input.substring(5);
                 System.out.println(echoOutput);
             }
-            if (Detector[0].equals("type")) {
+            else if (Detector[0].equals("type")) {
                 for (String command : commands) {
                     if (Detector[1].equals(command)) {
                         System.out.println(Detector[1] + " is a shell builtin");
